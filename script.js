20ff6772cf929e9f6900b8bb1e6ca964d5feb123
MANOJ.821B <mano.821b@gmail.com>
2025-02-27 18:49:45 +0530

    Added animations, hover effects, and mouse movement interactions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("mousemove", function (event) {
-    let x = (window.innerWidth / 2 - event.clientX) / 50;
-    let y = (window.innerHeight / 2 - event.clientY) / 50;
+    let x = (window.innerWidth / 2 - event.clientX) / 40;
+    let y = (window.innerHeight / 2 - event.clientY) / 40;
 
     document.querySelector(".hero").style.transform = `translate(${x}px, ${y}px)`;
 });
